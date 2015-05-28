8082659eba682ec27552fe51466729362747b63c
Chris Antaki <ChrisAntaki@gmail.com>
2015-05-28 15:09:59 -0700

    Checking for outdated browsers

--- js/breakcongress.js
+++ js/breakcongress.js
@@ -1,3 +1,18 @@
+// Check for outdated browsers
+var isIE = navigator.userAgent.match(/MSIE (\d+)\./);
+if (isIE) {
+    var version = +isIE[1];
+    if (version < 10) {
+        alert('Unfortunately your browser, Internet Explorer ' + version + ', is not supported.\nPlease visit the site with a modern browser like Firefox or Chrome.\nThanks!');
+    }
+}
+
+if (navigator.userAgent.match(/Android 2\.3/)) {
+    alert('Unfortunately your browser, Android 2.3, is not supported.\nPlease visit the site with a modern browser like Firefox or Chrome.\nThanks!');
+}
+
+
+
 var GLITCHINESS = 0.2;
 var MAX_COORD_GLITCH = 40;
 var COORD_GLITCH_MAX_ADJACENT = 10;
